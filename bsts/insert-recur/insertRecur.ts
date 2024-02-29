3f10ff4ecbc9eb3b8e0878486ec19955eb9f4228
Charles Chitty <charlescchitty@gmail.com>
2024-02-29 12:29:55 -0800

    Finished insertRecur

--- bsts/insert-recur/insertRecur.ts
+++ bsts/insert-recur/insertRecur.ts
@@ -1,30 +1,42 @@
-import { BNode, BSTNum } from "../common/bst";
+import { BNode, BNodeNum, BSTNum } from "../common/bst";
 
 /** insertRecursively(val): Insert a new node into the BST with value val.
  * Uses recursion. */
 
 function insertRecursively(bst: BSTNum, val: number): void {
-  const newNode = new BNode(val);
+
+  function insertRecursivelyNode(node: BNodeNum, val: number): void {
+    const newNode = new BNodeNum(val)
+    let curr = node
+
+    if (curr === null) return;
+
+    if (node.val < val){
+      if (curr.right === null) {
+        curr.right = newNode
+        return;
+      } else{
+        insertRecursivelyNode(curr.right, val)
+      }
+    } else if (val < node.val){
+      if (curr.left === null) {
+        curr.left = newNode;
+        return;
+      } else {
+        insertRecursivelyNode(curr.left, val)
+      }
+    }
+
+  }
+
+  if (bst.root === null) {
+    bst.root = new BNodeNum(val)
+  }
+
   let curNode = bst.root;
-  // base case
-  if (curNode === null) return;
 
-
-  //make a node instance
-  //if node's val is less than root -> go left
-  //if node's val is more than root -> go right
-
+  insertRecursivelyNode(curNode, val)
 }
 
-export { insertRecursively };
 
-/** Large tree for tests:
- *
- *               50
- *           /       \
- *        25           75
- *      /    \            \
- *   10       40           100
- *     \
- *      15
- */
+export { insertRecursively };
